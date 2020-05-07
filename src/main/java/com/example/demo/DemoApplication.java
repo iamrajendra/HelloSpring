16060b3fb2e73eeec079c64ab5d35410c3b21d4a
iamrajendra <rajendrarajaram@hotmail.com>
2020-05-08 01:34:48 +0530

    Building a RESTful Web Service https://spring.io/guides/gs/rest-service/

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.datatransfer.StringSelection;
 
-@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 
 @SpringBootApplication
 public class DemoApplication {
@@ -15,16 +15,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
-@RestController
-@RequestMapping(path="/")
-	public  class  DemoController {
-@RequestMapping(value = "print/{msg}",method = RequestMethod.GET)
-		public  String print (@PathVariable(value = "") String msg){
-
-			return  "Hello From  "+msg;
-		}
-
 	}
 
-}
+
+
