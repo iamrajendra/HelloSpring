58b4c4f7af876c9fcdecac3d1c973b9891934b14
iamrajendra <rajendrarajaram@hotmail.com>
2020-05-09 10:26:35 +0530

    Accessing MongoDB Data with REST This guide walks you through the process of creating an application that accesses document-based data through a hypermedia-based RESTful front end.
    https://spring.io/guides/gs/accessing-mongodb-data-rest/

--- src/main/java/com/example/demo/accessingmongodbdatarest/PersonRepository.java
+++ src/main/java/com/example/demo/accessingmongodbdatarest/PersonRepository.java
@@ -1,12 +1,13 @@
-package com.example.demo.hello;
+package com.example.demo.accessingmongodbdatarest;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import java.util.List;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
-public interface PersonRepository extends CrudRepository<Person, Long> {
+public interface PersonRepository extends MongoRepository<Person, String> {
 
     List<Person> findByLastName(@Param("name") String name);
 
